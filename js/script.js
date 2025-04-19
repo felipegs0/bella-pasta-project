fd4aef532d69ad182812f5cbf8320917f99046a4
Felipe Gomes <felipegstech@gmail.com>
2025-04-19 02:45:23 -0300

    uptade

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header")
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
+    if (window.scrollY > 100) {
         header.classList.add("ocult");
     } else {
         header.classList.remove("ocult")
